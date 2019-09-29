e9578a7f64cd8fcc4f05b6e5cad6eb20ef96cacd
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2019-09-29 19:46:05 -0300

    Updates

---
+++ precache-manifest.cdfa29aa15e8164169e3e863ae47be52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc762eda1078410fdcd903d5c8e48b3f",
+    "url": "/Quiz/index.html"
+  },
+  {
+    "revision": "222076bf13f97fef39c5",
+    "url": "/Quiz/static/css/main.97454ba2.chunk.css"
+  },
+  {
+    "revision": "9055c31327455321ec2a",
+    "url": "/Quiz/static/js/2.e189c643.chunk.js"
+  },
+  {
+    "revision": "222076bf13f97fef39c5",
+    "url": "/Quiz/static/js/main.1c37f412.chunk.js"
+  },
+  {
+    "revision": "4b9c606b86775ef80dd3",
+    "url": "/Quiz/static/js/runtime-main.ef56481e.js"
+  },
+  {
+    "revision": "d71fe07c2ddc838dad66a30b0f4ff9aa",
+    "url": "/Quiz/static/media/logo.d71fe07c.png"
+  }
+]);
